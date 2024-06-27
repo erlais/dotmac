5ed72de43047ca36ba8bda9bdb580ad1c08a392c
Erki L <erki@lsr.ee>
2024-06-28 01:25:09 +0300

    delete efm files

--- .config/nvim/lua/efm/black.lua
+++
@@ -1,8 +0,0 @@
-local cmd = "black --no-color -q --skip-string-normalization " ..
-            "$(echo ${--useless:rowStart} ${--useless:rowEnd} | xargs -n4 -r sh -c 'echo --line-ranges=$(($1+1))-$(($3+1))') -"
-
-return {
-  formatCanRange = true,
-  formatCommand = cmd,
-  formatStdin = true
-}
